86d9ca44e2f250d99c77457cbf9c952760247244
kevin <googlevsbing@126.com>
2015-04-23 10:20:26 +0800

    rename make dir

--- bash/bash_param.sh
+++ bash/bash_param.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
+# refer to http://www.ibm.com/developerworks/cn/linux/l-bash-parameters.html
+# and http://www.cnblogs.com/FrankTan/archive/2010/03/01/1634516.html
+
+
 # bash 中echo $#显示参数个数，不包括命令的名称
 # $@ $* "$*" 是一样的，都是参数的字符串形式，但"$@" 代表一个参数数组
 
